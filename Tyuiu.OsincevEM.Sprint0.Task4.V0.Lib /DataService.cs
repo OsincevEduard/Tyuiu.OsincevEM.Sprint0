80a91d21d85f6b8868052e546e4126f706ef102b
Eduard Osincev <Tyuiu.OsincevEM@outlook.com>
2026-09-17 01:05:17 +0500

    Task4

---
+++ Tyuiu.OsincevEM.Sprint0.Task4.V0.Lib /DataService.cs
@@ -0,0 +1,29 @@
+﻿namespace Tyuiu.OsincevEM.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        // Пример линейной структуры: сложение
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Пример линейной структуры: вычитание
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Пример линейной структуры: умножение
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Пример линейной структуры: деление
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
